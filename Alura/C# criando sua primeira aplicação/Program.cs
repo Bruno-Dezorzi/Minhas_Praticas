a635398be915e3b41a9fabf068a2248dd8cfe81c
Bruno Dezorzi <brunodezorzi9@gmail.com>
2025-02-17 18:08:52 -0300

    C# 1

---
+++ Alura/C# criando sua primeira aplicação/Program.cs
@@ -0,0 +1,25 @@
+﻿// Screen Sound
+
+
+
+void BoasVindas(){
+    string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
+    Console.WriteLine("************************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("************************************");
+}
+
+BoasVindas();
+
+
+class Carro{
+    private string marca;
+    private string modelo;
+    private string cor;
+
+    public Carro(string marca, string modelo, string cor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+}
